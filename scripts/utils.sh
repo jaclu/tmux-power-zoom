6c8b7f74f3225d61bc72b83346b1b9d6ebe0bc13
Jacob Lundqvist <jacob.lundqvist@gmail.com>
2022-02-04 17:36:52 +0100

    Added check_1_0_param()

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -3,9 +3,9 @@
 #   Copyright (c) 2022: Jacob.Lundqvist@gmail.com
 #   License: MIT
 #
-#   Part of https://github.com/jaclu/tmux-menus
+#   Part of https://github.com/jaclu/tmux-power-zoom
 #
-#   Version: 1.2.1 2022-02-03
+#   Version: 0.0.1 2022-02-04
 #
 #  Common stuff
 #
@@ -33,3 +33,27 @@ log_it() {
     printf "%s\n" "$@" >> "$log_file"
 }
 
+
+check_1_0_param() {
+    param_name="$1"
+
+    param_value=$(get_tmux_option "$param_name" "0")
+
+    case "$param_value" in
+        
+        "0" | "1" )  # expected params
+            param_verified="$param_value"
+	    ;;
+        
+        "yes" | "Yes" | "YES" | "true" | "True" | "TRUE" )
+	    #  Be a nice guy and accept some common positives
+            log_it "Converted incorret positive to 1"
+            param_verified=1
+            ;;
+        
+        *)
+            log_it "Invalid without_prefix value"
+            tmux display 'ERROR: "$param_name" should be 0 or 1, was: $param_value'
+            exit 0  # Exit 0 wont throw a tmux error            
+    esac
+}
