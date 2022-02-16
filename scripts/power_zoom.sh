8db8304d2d2d7ba7a8024504a41a8016e5fc5bd8
jaclu <jacob.lundqvist@gmail.com>
2022-02-16 01:03:02 +0100

    Fixed prevention of zooming the placeholder

--- scripts/power_zoom.sh
+++ scripts/power_zoom.sh
@@ -5,44 +5,66 @@
 #
 #   Part of https://github.com/jaclu/tmux-power-zoom
 #
-#   Version: 0.0.2 2022-02-04
+#   Version: 0.0.3 2022-02-16
 #
-#   This tracks the placeholder pane by its pane title, this works regardless
+#   Tracking the placeholder pane by its pane title, this works regardless
 #   if pane titles are displayed or not.
 #
+# shellcheck disable=SC1007
+CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
+
+# shellcheck disable=SC1091
+. "$CURRENT_DIR/utils.sh"
+
+
+
 power_zoom() {
+    log_it "power_zoom() triggered" 
+
     primary_pane_id="$(tmux display -p '#D')"
     primary_pane_title="$(tmux display -p '#T')"
 
-    placeholder_title="=== POWER ZOOM === place-holder for pane: $primary_pane_id"
+    placeholder_stub="=== POWER ZOOM === place-holder for pane:"
+    placeholder_title="$placeholder_stub $primary_pane_id"
+
+    log_it "Checking for this place holder: [$placeholder_title]"
     placeholder_pane=$(tmux list-panes -a -F "#D #T" | grep "$placeholder_title" | awk '{ print $1 }')
 
     if [ -n "$placeholder_pane" ]; then
         #
-        #  Un-Zoom - move pane back to original location
+        #  Found a place holder for current pane, move it there and delete
+        #  the place holder
         #
+        log_it "Found a matching place holder, move this pane there and delete it"
         tmux join-pane -b -t "$placeholder_pane"
         tmux kill-pane -t "$placeholder_pane"
     else
         #
-        #  Zoom it
+        #  Zoom this to new window
         #
         if [ "$(tmux list-panes | wc -l)" -eq 1 ]; then
-            tmux display "Only one pane in this window!"
+            msg="Cant zoom only pane in a window"
+            log_it "$msg"
+            tmux display "$msg"
             return
         fi
-        if [ "$(tmux display -p '#{pane_dead}')" -eq 1 ]; then
-            tmux display "This is a Power-Zoom placeholder!"
+        if [ "$(tmux display -p '#T' | grep "$placeholder_stub")" != "" ]; then
+            msg="This is a Power-Zoom place holder!"
+            log_it "$msg"
+            tmux display "$msg"
             return
         fi
-	#
-	# the pane would close when the process is terminated, so keep a long sleep going for ever
-	# Ctrl-C would exit script and pane would close.
-	#
+        #
+        #  the place holder pane will close when it's process is terminated, 
+        #  so keep a long sleep going for ever in a loop.
+        #  Ctrl-C would exit script and pane would close in case the zoomed pane
+        #  is killed and the place holder is left hanging.
+        #
+        log_it "Zoom active pane to new window"
         tmux split-window -b "echo; echo \"  $placeholder_title\"; while true ; do sleep 30; done"
         tmux select-pane -T "$placeholder_title"
         tmux select-pane -t "$primary_pane_id"
-        tmux break-pane
+        tmux break-pane  # move it to new window
         tmux rename-window "**POWER ZOOM** $primary_pane_title ($primary_pane_id)"
     fi
 }
