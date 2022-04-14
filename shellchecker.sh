cb1baa0d19ba917b390fcfbaa848fafbf079e33c
jaclu <jacob.lundqvist@gmail.com>
2022-04-14 03:48:43 +0200

    Runs shellcheck on all relevant scripts

--- shellchecker.sh
+++ shellchecker.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/env bash
 #
 #   Copyright (c) 2022: Jacob.Lundqvist@gmail.com
 #   License: MIT
 #
 #   Part of https://github.com/jaclu/tmux-power-zoom
 #
-#   Version: 1.0.0 2022-04-14
+#   Version: 1.0.1 2022-04-14
 #
 #   Does shellcheck on all relevant scripts in this project
 #
@@ -13,8 +13,22 @@
 # shellcheck disable=SC1007
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 
-#  Obviousl self exam should be done :)
-shellcheck "$CURRENT_DIR"/shellchecker.sh
+cd "$CURRENT_DIR" || return
 
-shellcheck "$CURRENT_DIR"/power-zoom.tmux
-shellcheck "$CURRENT_DIR"/scripts/*.sh
+
+checkables=(
+
+    #  Obviousl self exam should be done :)
+    shellchecker.sh
+
+    power-zoom.tmux
+
+    scripts/*.sh
+)
+
+for script in "${checkables[@]}"; do
+    # abort as soon as one gives warnings
+    echo "Cecking: $script"
+    shellcheck -x "$script" || exit 1
+
+done
